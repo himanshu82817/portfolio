dce80e89b6d113a1b85967e589d0e35c0e3534aa
himanshu82817 <himanshu82817@gmail.com>
2025-04-14 00:27:45 +0530

    projects added

--- src/app/light-sky/light-sky.component.ts
+++ src/app/light-sky/light-sky.component.ts
@@ -12,52 +12,52 @@ export class LightSkyComponent implements AfterViewInit {
   private numClouds = 6;
 
   ngAfterViewInit() {
-    this.canvas = document.getElementById("lightSky") as HTMLCanvasElement;
-    this.ctx = this.canvas.getContext("2d")!;
-    this.resizeCanvas();
+    // this.canvas = document.getElementById("lightSky") as HTMLCanvasElement;
+    // this.ctx = this.canvas.getContext("2d")!;
+    // this.resizeCanvas();
 
-    this.generateClouds();
-    this.animate();
+    // this.generateClouds();
+    // this.animate();
   }
 
-  @HostListener('window:resize')
-  resizeCanvas() {
-    this.canvas.width = window.innerWidth;
-    this.canvas.height = window.innerHeight;
-  }
+  // @HostListener('window:resize')
+  // resizeCanvas() {
+  //   this.canvas.width = window.innerWidth;
+  //   this.canvas.height = window.innerHeight;
+  // }
 
-  generateClouds() {
-    this.clouds = [];
-    for (let i = 0; i < this.numClouds; i++) {
-      this.clouds.push({
-        x: Math.random() * this.canvas.width,
-        y: Math.random() * (this.canvas.height / 2),
-        width: Math.random() * 150 + 100,
-        height: Math.random() * 50 + 30,
-        speed: Math.random() * 1 + 0.5,
-        opacity: Math.random() * 0.5 + 0.3
-      });
-    }
-  }
+  // generateClouds() {
+  //   this.clouds = [];
+  //   for (let i = 0; i < this.numClouds; i++) {
+  //     this.clouds.push({
+  //       x: Math.random() * this.canvas.width,
+  //       y: Math.random() * (this.canvas.height / 2),
+  //       width: Math.random() * 150 + 100,
+  //       height: Math.random() * 50 + 30,
+  //       speed: Math.random() * 1 + 0.5,
+  //       opacity: Math.random() * 0.5 + 0.3
+  //     });
+  //   }
+  // }
 
-  drawClouds() {
-    this.ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
-    this.clouds.forEach((cloud) => {
-      this.ctx.beginPath();
-      this.ctx.ellipse(cloud.x, cloud.y, cloud.width, cloud.height, 0, 0, Math.PI * 2);
-      this.ctx.fill();
-      cloud.x += cloud.speed;
+  // drawClouds() {
+  //   this.ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
+  //   this.clouds.forEach((cloud) => {
+  //     this.ctx.beginPath();
+  //     this.ctx.ellipse(cloud.x, cloud.y, cloud.width, cloud.height, 0, 0, Math.PI * 2);
+  //     this.ctx.fill();
+  //     cloud.x += cloud.speed;
 
-      // Loop clouds to the left when they exit the screen
-      if (cloud.x > this.canvas.width + 50) {
-        cloud.x = -cloud.width;
-      }
-    });
-  }
+  //     // Loop clouds to the left when they exit the screen
+  //     if (cloud.x > this.canvas.width + 50) {
+  //       cloud.x = -cloud.width;
+  //     }
+  //   });
+  // }
 
-  animate() {
-    requestAnimationFrame(() => this.animate());
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.drawClouds();
-  }
+  // animate() {
+  //   requestAnimationFrame(() => this.animate());
+  //   this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  //   this.drawClouds();
+  // }
 }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,11 +1,264 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
+    activeItem = -1;
 
+    projects = [
+      {
+        images:[
+          "assets/profile.png"
+        ],
+        heading:'Loan Origination System (SMC Finance)',
+        techStack:[
+          {
+            name:"Angular",
+            icon:"assets/skills/Angular.png"
+          },
+          {
+            name:"NestJS",
+            icon:"assets/skills/Nestjs.png"
+          },
+          {
+            name:"SQL Server",
+            icon:"assets/skills/SQLServer.png"
+          },
+          {
+            name:"GCP",
+            icon:"assets/skills/GCP.png"
+          },
+          {
+            name:"TypeORM",
+            icon:"assets/skills/Typeorm.png"
+          },
+        ],
+        about:{
+          description:"Worked as a Full Stack Developer on a robust Loan Origination System (LOS) tailored for internal teams at SMC Finance. The platform streamlined the loan processing workflow—from lead creation to approval and disbursement—enhancing efficiency and compliance."
+        },
+        contributions:[
+          "Developed and maintained multiple modules including Lead Management, Document Verification, Credit Analysis, and Loan Approval.",
+          "Designed and implemented dynamic forms using Angular Reactive Forms for customer onboarding.",
+          "Built secure and scalable RESTful APIs in NestJS for data validation, document storage, and workflow automation.",
+          "Integrated SQL Server using TypeORM, optimizing queries and managing relationships between loan applications, users, and transactions.",
+          "Collaborated with credit teams to build custom dashboards for tracking application status and KPIs.",
+          "Deployed modules on Google Cloud Platform, ensuring availability and reliability.",
+        ],
+        summery:'This system significantly reduced manual processing, minimized errors, and improved turnaround time for loan approvals.'
+      },
+      {
+        images:[
+          "assets/profile.png"
+        ],
+        heading:'Loan Origination System (SMC Finance)',
+        techStack:[
+          {
+            name:"Angular",
+            icon:"assets/skills/Angular.png"
+          },
+          {
+            name:"NestJS",
+            icon:"assets/skills/Nestjs.png"
+          },
+          {
+            name:"SQL Server",
+            icon:"assets/skills/SQLServer.png"
+          },
+          {
+            name:"GCP",
+            icon:"assets/skills/GCP.png"
+          },
+          {
+            name:"TypeORM",
+            icon:"assets/skills/Typeorm.png"
+          },
+        ],
+        about:{
+          description:"Worked as a Full Stack Developer on a robust Loan Origination System (LOS) tailored for internal teams at SMC Finance. The platform streamlined the loan processing workflow—from lead creation to approval and disbursement—enhancing efficiency and compliance."
+        },
+        contributions:[
+          "Developed and maintained multiple modules including Lead Management, Document Verification, Credit Analysis, and Loan Approval.",
+          "Designed and implemented dynamic forms using Angular Reactive Forms for customer onboarding.",
+          "Built secure and scalable RESTful APIs in NestJS for data validation, document storage, and workflow automation.",
+          "Integrated SQL Server using TypeORM, optimizing queries and managing relationships between loan applications, users, and transactions.",
+          "Collaborated with credit teams to build custom dashboards for tracking application status and KPIs.",
+          "Deployed modules on Google Cloud Platform, ensuring availability and reliability.",
+        ],
+        summery:'This system significantly reduced manual processing, minimized errors, and improved turnaround time for loan approvals.'
+      },
+      {
+        images:[
+          "assets/profile.png"
+        ],
+        heading:'Loan Origination System (SMC Finance)',
+        techStack:[
+          {
+            name:"Angular",
+            icon:"assets/skills/Angular.png"
+          },
+          {
+            name:"NestJS",
+            icon:"assets/skills/Nestjs.png"
+          },
+          {
+            name:"SQL Server",
+            icon:"assets/skills/SQLServer.png"
+          },
+          {
+            name:"GCP",
+            icon:"assets/skills/GCP.png"
+          },
+          {
+            name:"TypeORM",
+            icon:"assets/skills/Typeorm.png"
+          },
+        ],
+        about:{
+          description:"Worked as a Full Stack Developer on a robust Loan Origination System (LOS) tailored for internal teams at SMC Finance. The platform streamlined the loan processing workflow—from lead creation to approval and disbursement—enhancing efficiency and compliance."
+        },
+        contributions:[
+          "Developed and maintained multiple modules including Lead Management, Document Verification, Credit Analysis, and Loan Approval.",
+          "Designed and implemented dynamic forms using Angular Reactive Forms for customer onboarding.",
+          "Built secure and scalable RESTful APIs in NestJS for data validation, document storage, and workflow automation.",
+          "Integrated SQL Server using TypeORM, optimizing queries and managing relationships between loan applications, users, and transactions.",
+          "Collaborated with credit teams to build custom dashboards for tracking application status and KPIs.",
+          "Deployed modules on Google Cloud Platform, ensuring availability and reliability.",
+        ],
+        summery:'This system significantly reduced manual processing, minimized errors, and improved turnaround time for loan approvals.'
+      },
+      {
+        images:[
+          "assets/profile.png"
+        ],
+        heading:'Loan Origination System (SMC Finance)',
+        techStack:[
+          {
+            name:"Angular",
+            icon:"assets/skills/Angular.png"
+          },
+          {
+            name:"NestJS",
+            icon:"assets/skills/Nestjs.png"
+          },
+          {
+            name:"SQL Server",
+            icon:"assets/skills/SQLServer.png"
+          },
+          {
+            name:"GCP",
+            icon:"assets/skills/GCP.png"
+          },
+          {
+            name:"TypeORM",
+            icon:"assets/skills/Typeorm.png"
+          },
+        ],
+        about:{
+          description:"Worked as a Full Stack Developer on a robust Loan Origination System (LOS) tailored for internal teams at SMC Finance. The platform streamlined the loan processing workflow—from lead creation to approval and disbursement—enhancing efficiency and compliance."
+        },
+        contributions:[
+          "Developed and maintained multiple modules including Lead Management, Document Verification, Credit Analysis, and Loan Approval.",
+          "Designed and implemented dynamic forms using Angular Reactive Forms for customer onboarding.",
+          "Built secure and scalable RESTful APIs in NestJS for data validation, document storage, and workflow automation.",
+          "Integrated SQL Server using TypeORM, optimizing queries and managing relationships between loan applications, users, and transactions.",
+          "Collaborated with credit teams to build custom dashboards for tracking application status and KPIs.",
+          "Deployed modules on Google Cloud Platform, ensuring availability and reliability.",
+        ],
+        summery:'This system significantly reduced manual processing, minimized errors, and improved turnaround time for loan approvals.'
+      },
+      {
+        images:[
+          "assets/profile.png"
+        ],
+        heading:'Loan Origination System (SMC Finance)',
+        techStack:[
+          {
+            name:"Angular",
+            icon:"assets/skills/Angular.png"
+          },
+          {
+            name:"NestJS",
+            icon:"assets/skills/Nestjs.png"
+          },
+          {
+            name:"SQL Server",
+            icon:"assets/skills/SQLServer.png"
+          },
+          {
+            name:"GCP",
+            icon:"assets/skills/GCP.png"
+          },
+          {
+            name:"TypeORM",
+            icon:"assets/skills/Typeorm.png"
+          },
+        ],
+        about:{
+          description:"Worked as a Full Stack Developer on a robust Loan Origination System (LOS) tailored for internal teams at SMC Finance. The platform streamlined the loan processing workflow—from lead creation to approval and disbursement—enhancing efficiency and compliance."
+        },
+        contributions:[
+          "Developed and maintained multiple modules including Lead Management, Document Verification, Credit Analysis, and Loan Approval.",
+          "Designed and implemented dynamic forms using Angular Reactive Forms for customer onboarding.",
+          "Built secure and scalable RESTful APIs in NestJS for data validation, document storage, and workflow automation.",
+          "Integrated SQL Server using TypeORM, optimizing queries and managing relationships between loan applications, users, and transactions.",
+          "Collaborated with credit teams to build custom dashboards for tracking application status and KPIs.",
+          "Deployed modules on Google Cloud Platform, ensuring availability and reliability.",
+        ],
+        summery:'This system significantly reduced manual processing, minimized errors, and improved turnaround time for loan approvals.'
+      },
+      {
+        images:[
+          "assets/profile.png"
+        ],
+        heading:'Loan Origination System (SMC Finance)',
+        techStack:[
+          {
+            name:"Angular",
+            icon:"assets/skills/Angular.png"
+          },
+          {
+            name:"NestJS",
+            icon:"assets/skills/Nestjs.png"
+          },
+          {
+            name:"SQL Server",
+            icon:"assets/skills/SQLServer.png"
+          },
+          {
+            name:"GCP",
+            icon:"assets/skills/GCP.png"
+          },
+          {
+            name:"TypeORM",
+            icon:"assets/skills/Typeorm.png"
+          },
+        ],
+        about:{
+          description:"Worked as a Full Stack Developer on a robust Loan Origination System (LOS) tailored for internal teams at SMC Finance. The platform streamlined the loan processing workflow—from lead creation to approval and disbursement—enhancing efficiency and compliance."
+        },
+        contributions:[
+          "Developed and maintained multiple modules including Lead Management, Document Verification, Credit Analysis, and Loan Approval.",
+          "Designed and implemented dynamic forms using Angular Reactive Forms for customer onboarding.",
+          "Built secure and scalable RESTful APIs in NestJS for data validation, document storage, and workflow automation.",
+          "Integrated SQL Server using TypeORM, optimizing queries and managing relationships between loan applications, users, and transactions.",
+          "Collaborated with credit teams to build custom dashboards for tracking application status and KPIs.",
+          "Deployed modules on Google Cloud Platform, ensuring availability and reliability.",
+        ],
+        summery:'This system significantly reduced manual processing, minimized errors, and improved turnaround time for loan approvals.'
+      },
+    ]
+
+
+    readMore(index:number){
+      this.activeItem = index
+    }
+
+    clear(){
+      this.activeItem = -1
+    }
 }
